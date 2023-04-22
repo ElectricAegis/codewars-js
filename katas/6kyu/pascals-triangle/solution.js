bafae672a7eb886c3091279df9e6b54e6949e60b
Manie <manie.brummer@vcontractor.co.za>
2023-04-22 11:00:31 +0200

    minor refactor

--- katas/6kyu/pascals-triangle/solution.js
+++ katas/6kyu/pascals-triangle/solution.js
@@ -1,14 +1,17 @@
-function pascalsTriangle(n) {
-  //return a flat array representing the values of Pascal's Triangle to the n-th level
-  if (n == 1) return [1];
-  let prevArrays = pascalsTriangle(n-1);
-  let prevArray = prevArrays.slice(prevArrays.length - (n - 1));
-  let newArray = [1];
-  for (let i = 0; i < n-2; i++) {
-    newArray.push(prevArray[i] + prevArray[i+1]);
+function calculateNextRow(lastRow) {
+  let nextRow = [];
+  for (let i = 0; i < lastRow.length - 1; i++) {
+    nextRow.push(lastRow[i] + lastRow[i + 1]);
   }
-  newArray.push(1);
-  return [...prevArrays, ...newArray];
+  return [1, ...nextRow, 1];
+}
+
+function pascalsTriangle(n) {
+  if (n === 1) return [1];
+  let previousTriangle = pascalsTriangle(n - 1);
+  let lastRow = previousTriangle.slice(previousTriangle.length - (n - 1));
+
+  return [...previousTriangle, ...(calculateNextRow(lastRow))];
 }
 
 module.exports = pascalsTriangle;
