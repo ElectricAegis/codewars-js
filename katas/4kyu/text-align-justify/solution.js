9018b600282270398dc9cc9423b784db7e36650a
Manie <manie.brummer@vcontractor.co.za>
2023-04-26 21:21:38 +0200

    more regex

--- katas/4kyu/text-align-justify/solution.js
+++ katas/4kyu/text-align-justify/solution.js
@@ -1,33 +1,26 @@
+function getSpaceExpression(num) {
+  return new RegExp(`(\\S)(\\s{${num}})(\\S)`);
+}
 function justify(text, width) {
-  let regex = new RegExp(`(?!.{1,${width}}$)(.{1,${width}})\\s`, 'g');
-  let wrappedText = text.replace(regex, '$1\n');
+  let wrapExpression = new RegExp(`(?!.{1,${width}}$)(.{1,${width}})\\s`, 'g');
+  let wrappedText = text.replace(wrapExpression, '$1\n');
   let wrappedArray = wrappedText.split('\n');
 
-  let newText = '';
-  for(let i = 0; i < wrappedArray.length - 1; i++){
-    let wordArray = wrappedArray[i].split(' ').filter(x =>x.length > 0);
-    let lineArray = [{word: wordArray.shift(), isSpace: false}];
-    while (wordArray.length != 0) {
-      lineArray.push({word: ' ', isSpace: true});
-      lineArray.push({ word: wordArray.shift(), isSpace: false})
-    }
+  for (let i = 0; i < wrappedArray.length - 1; i++) {
 
-    if(lineArray.length === 1) {
-      newText += wrappedArray[i] + '\n';
-    } else {
-      let spaces = lineArray.filter(x => x.isSpace);
-      while (lineArray.reduce((accumulator, nextValue) => accumulator + nextValue.word.length, 0) < width) {
-        let space = spaces.shift();
-        space.word +=  ' ';
-        if (spaces.length === 0) {
-          spaces = lineArray.filter(x => x.isSpace);
+    if (wrappedArray[i].indexOf(' ') !== -1) {
+      let spaceSize = 1;
+      while (wrappedArray[i].length < width) {
+        let originalLength = wrappedArray[i].length;
+        wrappedArray[i] = wrappedArray[i].replace(getSpaceExpression(spaceSize), '$1$2 $3');
+        if (originalLength === wrappedArray[i].length) {
+          spaceSize += 1;
         }
       }
-      newText += lineArray.map(x => x.word).join('') + '\n';
     }
   }
-  newText += wrappedArray[wrappedArray.length-1];
-  return newText;
+  return wrappedArray.join('\n');
 }
+
 
 module.exports = justify;
