ac35cc848dd5279135e59486a61e6df840c93b9a
Manie <manie.brummer@vcontractor.co.za>
2023-04-26 21:44:41 +0200

    minor clean up

--- katas/4kyu/text-align-justify/solution.js
+++ katas/4kyu/text-align-justify/solution.js
@@ -1,13 +1,11 @@
-function getSpaceExpression(num) {
-  return new RegExp(`(\\S)(\\s{${num}})(\\S)`);
-}
+const getSpaceExpression = num => new RegExp(`(\\S)(\\s{${num}})(\\S)`);
+
 function justify(text, width) {
   let wrapExpression = new RegExp(`(?!.{1,${width}}$)(.{1,${width}})\\s`, 'g');
   let wrappedText = text.replace(wrapExpression, '$1\n');
   let wrappedArray = wrappedText.split('\n');
 
   for (let i = 0; i < wrappedArray.length - 1; i++) {
-
     if (wrappedArray[i].indexOf(' ') !== -1) {
       let spaceSize = 1;
       while (wrappedArray[i].length < width) {
@@ -21,6 +19,5 @@ function justify(text, width) {
   }
   return wrappedArray.join('\n');
 }
-
 
 module.exports = justify;
