38bcade43b27a38631e178bee5b08fb260153cc9
Manie <manie.brummer@vcontractor.co.za>
2023-04-26 20:30:36 +0200

    a solution using regex

--- katas/4kyu/text-align-justify/solution.js
+++ katas/4kyu/text-align-justify/solution.js
@@ -1,31 +1,32 @@
 function justify(text, width) {
-  let textArray = text.split(' ');
+  let regex = new RegExp(`(?!.{1,${width}}$)(.{1,${width}})\\s`, 'g');
+  let wrappedText = text.replace(regex, '$1\n');
+  let wrappedArray = wrappedText.split('\n');
+
   let newText = '';
-  while (textArray.length !== 0) {
-    let lineArray = [{word: textArray.shift(), type: 'word'}];
-    let lineLength = lineArray[0].word.length;
-    while (textArray.length !== 0 && (lineLength + textArray[0].length + 1 <= width)) {
-      let word = textArray.shift();
-      lineArray.push({word: ' ', type: 'space'});
-      lineArray.push({word: word, type: 'word'});
-      lineLength += (word.length + 1);
+  for(let i = 0; i < wrappedArray.length - 1; i++){
+    let wordArray = wrappedArray[i].split(' ').filter(x =>x.length > 0);
+    let lineArray = [{word: wordArray.shift(), isSpace: false}];
+    while (wordArray.length != 0) {
+      lineArray.push({word: ' ', isSpace: true});
+      lineArray.push({ word: wordArray.shift(), isSpace: false})
     }
-    if (lineLength <= width && textArray.length === 0) {
-      newText += lineArray.map(x => x.word).join('');
-    } else if (lineArray.filter(x => x.type === 'space').length === 0) {
-      newText += lineArray.map(x => x.word).join('') + '\n';
+
+    if(lineArray.length === 1) {
+      newText += wrappedArray[i] + '\n';
     } else {
-      let spaces = lineArray.filter(x => x.type === 'space');
+      let spaces = lineArray.filter(x => x.isSpace);
       while (lineArray.reduce((accumulator, nextValue) => accumulator + nextValue.word.length, 0) < width) {
         let space = spaces.shift();
         space.word +=  ' ';
         if (spaces.length === 0) {
-          spaces = lineArray.filter(x => x.type === 'space');
+          spaces = lineArray.filter(x => x.isSpace);
         }
       }
       newText += lineArray.map(x => x.word).join('') + '\n';
     }
   }
+  newText += wrappedArray[wrappedArray.length-1];
   return newText;
 }
 
